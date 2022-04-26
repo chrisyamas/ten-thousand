9ad639ce9e5ea96452f1219f510bf682bb48fe7b
Christopher Yamas <yamas.chris@gmail.com>
2022-04-25 22:44:05 -0400

    README add

---
+++ README.md
@@ -0,0 +1,21 @@
+# LAB - Class 06
+
+## Project: Ten Thousand Game
+
+- This is a version of the game Ten Thousand. Today we tackled keeping track of the score, rolling the dice, and banking of points.
+
+### Author: Ella Svete & Christopher Yamas
+
+### Links and Resources
+
+- back-end server url (when applicable)
+- front-end application (when applicable)
+
+### How to initialize/run your application (where applicable)
+
+- We initialize our application by running pytest. User must have a .venv and pytest installed.
+
+### Tests
+
+- We run tests by running pytest
+- The tests for this application were provided for us and we did not get to writing our own.
--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,10 +1,13 @@
 import random
+from collections import Counter
 
 
 class GameLogic:
 
-    def calculate_score(self):
+    def __init__(self):
         pass
+
+    number_of = 0
 
     @staticmethod
     def roll_dice(num):
@@ -13,8 +16,71 @@ class GameLogic:
             rolls_list.append(random.randint(1, 6))
         return tuple(rolls_list)
 
+    @staticmethod
+    def calculate_score(roll):
+        multiple = 0
+        user_score = 0
+        count = Counter(roll)
+        count_return = count.most_common()
+
+        if len(count_return) == 6:
+            GameLogic.number_of = 6
+            return 1500
+
+        if len(roll) == 0:
+            return user_score
+
+        if len(count_return) == 3 and len(roll) == 6:
+            for x in range(3):
+                if count_return[x][1] == 2:
+                    multiple += 1
+
+        if multiple == 3:
+            GameLogic.number_of = 6
+            return 1500
+
+        else:
+            for x in range(len(count_return)):
+                dots = count_return[x][0]
+                default_unit = dots * 100
+                occurrence = count_return[x][1]
+
+                if dots == 1:
+                    if occurrence > 2:
+                        default_unit = dots * 1000
+
+                    else:
+                        user_score += default_unit * occurrence
+
+                if dots == 5:
+                    if occurrence < 3:
+                        user_score += dots * occurrence * 10
+
+                if occurrence > 2:
+                    user_score += (occurrence - 2) * default_unit
+
+        return user_score
+
 
 class Banker:
+    def __init__(self):
+        self.balance = 0
+        self.shelved = 0
 
-    def __init__(self, shelf, bank, clear_shelf):
-        pass
+    # when you roll the dice, you want to keep these numbers
+    def shelf(self, number):
+        self.shelved += number
+        return self.shelved
+
+    # add points on the shelf to the total and reset
+
+    def bank(self):
+        self.balance += self.shelved
+        self.shelved = 0
+        return self.balance
+
+    # remove all un-banked points
+
+    def clear_shelf(self):
+        self.shelved = 0
+        return self.shelved
